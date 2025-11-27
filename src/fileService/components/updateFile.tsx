06dc694897b3dd91b53b5a5ad863c70723bdd4ba
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-11-27 06:26:18 +0000

    redirección depues de subir archivo

--- src/fileService/components/updateFile.tsx
+++ src/fileService/components/updateFile.tsx
@@ -1,15 +1,19 @@
 import { useSaveFile } from "../hooks";
 import { useCallback } from "react";
+import { useNavigate } from "react-router";
 
 export default function UpdateFile() {
+    const navigate = useNavigate();
     const saveFile = useSaveFile();
     const updateFile = useCallback(async (e) => {
         const file = e.target?.files?.[0];
         if (!file) return;
         const formData = new FormData();
         formData.append('file', file, file.name);
-        await saveFile.mutateAsync(formData);
-    }, [saveFile]);
+        const fileResponse = await saveFile.mutateAsync(formData);
+        console.log(fileResponse);
+        navigate(`/quoter/${fileResponse}`);
+    }, [saveFile, navigate]);
 
     return (
         <input
