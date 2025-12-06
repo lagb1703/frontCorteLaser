3f682c736e94fc661ffd40b11fa0fe8510debdbc
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-12-06 02:58:29 +0000

    agregado mejoras en la ui

--- src/userModule/components/userReview.tsx
+++ src/userModule/components/userReview.tsx
@@ -1,8 +1,8 @@
 import { useInfo } from "../hooks";
-import { type User } from "../validators/userValidators";
 import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface props {
     disable?: boolean;
@@ -13,91 +13,105 @@ export default function UserReview({ disable = false }: props) {
     console.log(form.formState.errors);
     return (
         <Form {...form}>
-            <form noValidate>
-                <h2>Editar Usuario</h2>
+            <form
+                className="space-y-6 w-full max-w-lg p-2"
+                noValidate>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Información del Usuario</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div
+                            className="flex flex-wrap w-full justify-between">
+                            <FormField
+                                control={form.control}
+                                name="names"
+                                render={({ field }) => (
+                                    <FormItem
+                                        className="basis-full lg:basis-[48%]">
+                                        <FormLabel>Nombres</FormLabel>
+                                        <FormControl>
+                                            <Input id="names" {...field} disabled={disable || true} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
 
-                <FormField
-                    control={form.control}
-                    name="names"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Nombres</FormLabel>
-                            <FormControl>
-                                <Input id="names" {...field} disabled={disable} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                            <FormField
+                                control={form.control}
+                                name="lastNames"
+                                render={({ field }) => (
+                                    <FormItem
+                                        className="basis-full lg:basis-[48%]">
+                                        <FormLabel>Apellidos</FormLabel>
+                                        <FormControl>
+                                            <Input id="lastNames" {...field} disabled={disable || true} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
 
-                <FormField
-                    control={form.control}
-                    name="lastNames"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Apellidos</FormLabel>
-                            <FormControl>
-                                <Input id="lastNames" {...field} disabled={disable} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Correo</FormLabel>
+                                    <FormControl>
+                                        <Input id="email" type="email" {...field} disabled={disable || true} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
 
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Correo</FormLabel>
-                            <FormControl>
-                                <Input id="email" type="email" {...field} disabled={disable} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
+                        <div
+                            className="flex flex-wrap w-full justify-between">
+                            <FormField
+                                control={form.control}
+                                name="address"
+                                render={({ field }) => (
+                                    <FormItem
+                                        className="basis-full lg:basis-[48%]">
+                                        <FormLabel>Dirección</FormLabel>
+                                        <FormControl>
+                                            <Input id="address" {...field} disabled={disable} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
 
-                <FormField
-                    control={form.control}
-                    name="address"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Dirección</FormLabel>
-                            <FormControl>
-                                <Input id="address" {...field} disabled={disable} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-
-                <FormField
-                    control={form.control}
-                    name="phone"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Teléfono</FormLabel>
-                            <FormControl>
-                                <Input id="phone" type="tel" {...field} disabled={disable} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-
-                {!disable && (
-                    <div>
-                        <Button
-                            onClick={form.handleSubmit(handleSummit)}
-
-                            disabled={changeStatus === "pending"}
-                            className="inline-flex items-center justify-center gap-2 rounded-md bg-primary text-primary-foreground px-4 py-2 text-sm font-medium disabled:opacity-50"
-                        >
-                            {changeStatus === "pending" ? "Guardando..." : "Guardar cambios"}
-                        </Button>
-                    </div>
-                )}
+                            <FormField
+                                control={form.control}
+                                name="phone"
+                                render={({ field }) => (
+                                    <FormItem
+                                        className="basis-full lg:basis-[48%]">
+                                        <FormLabel>Teléfono</FormLabel>
+                                        <FormControl>
+                                            <Input id="phone" type="tel" {...field} disabled={disable || true} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div
+                            className="flex w-full justify-center mt-4">
+                            <Button
+                                onClick={form.handleSubmit(handleSummit)}
+                                disabled={changeStatus === "pending" || disable}
+                                className="inline-flex items-center justify-center gap-2 rounded-md bg-primary text-primary-foreground px-4 py-2 text-sm font-medium disabled:opacity-50"
+                            >
+                                {changeStatus === "pending" ? "Guardando..." : "Guardar cambios"}
+                            </Button>
+                        </div>
+                    </CardContent>
+                </Card>
             </form>
         </Form>
     );
--- src/userModule/pages/userPage.tsx
+++ src/userModule/pages/userPage.tsx
@@ -3,8 +3,14 @@ import UserReview from "../components/userReview";
 export default function UserPage() {
     return (
         <div>
-            <h1>Perfil de Usuario</h1>
-            <UserReview disable={false} />
+            <h1
+                className="text-4xl font-bold my-6 text-center">
+                Perfil de Usuario
+            </h1>
+            <main
+                className="flex justify-center">
+                <UserReview disable={false} />
+            </main>
         </div>
     );
 }
