3e153ab180d26569343e7f0425ee163ceeb63f14
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-12-11 04:38:02 +0000

    agregado limpiado de cache

--- src/utilities/hooks/useLogOut.tsx
+++ src/utilities/hooks/useLogOut.tsx
@@ -1,10 +1,19 @@
 import { FetchWapper } from "@/utilities/fecth";
 import { useCallback, useRef } from "react";
+import { useQueryClient } from '@tanstack/react-query'
 
 export function useLogOut() {
+    const queryClient = useQueryClient();
     const fetchWrapper = useRef<FetchWapper>(FetchWapper.getInstance());
     const logOut = useCallback(() => {
         fetchWrapper.current.setToken(null);
-    }, [fetchWrapper]);
+        try {
+            queryClient.getQueryCache().clear();
+            queryClient.getMutationCache().clear();
+        } catch (e) {
+            // In case those methods are unavailable, ensure queries are removed
+        }
+        queryClient.removeQueries();
+    }, [fetchWrapper, queryClient]);
     return { logOut };
 }
