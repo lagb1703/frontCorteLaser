d344b1c472a8724940eba186f9bcb1d03418d1bc
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2026-01-21 02:23:54 +0000

    cambiado resumeAll a card de shadcn

--- src/fileService/components/resumeAll.tsx
+++ src/fileService/components/resumeAll.tsx
@@ -17,23 +17,25 @@ export default function ResumeAll({ prices }: ResumeAllProps) {
     const disable = prices.reduce((acc, curr) => acc && (curr.amount > 0 && !!curr.materialId && !!curr.thicknessId), prices.length > 0);
     const { isOpen: isPaymentOpen, open: openPayment, close: closePayment } = useOpenClose();
     return (
-        <div className="w-full h-full flex flex-col justify-between p-4 border rounded-lg shadow-md">
-            <h2 className="text-xl font-bold mb-4">Resumen de la compra</h2>
-            <div>
-                <div className="flex justify-between font-bold text-lg">
-                    <span>Total:</span>
-                    <span>${totalPrice.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                </div>
-                <Button className="mt-4 w-full" onClick={openPayment} disabled={!disable}>Proceder al pago</Button>
-                <PaymentDialog isOpen={isPaymentOpen} onClose={closePayment} items={prices.map(price=>{
-                    return {
-                        fileId: price.fileId,
-                        amount: price.amount,
-                        materialId: price.materialId,
-                        thicknessId: price.thicknessId
-                    }
-                })} />
+        <Card className="w-full h-full flex flex-col justify-between shadow-md">
+            <CardHeader>
+            <CardTitle className="text-xl font-bold">Resumen de la compra</CardTitle>
+            </CardHeader>
+            <CardContent>
+            <div className="flex justify-between font-bold text-lg">
+                <span>Total:</span>
+                <span>${totalPrice.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
             </div>
-        </div>
+            <Button className="mt-4 w-full" onClick={openPayment} disabled={!disable}>Proceder al pago</Button>
+            <PaymentDialog isOpen={isPaymentOpen} onClose={closePayment} items={prices.map(price => {
+                return {
+                fileId: price.fileId,
+                amount: price.amount,
+                materialId: price.materialId,
+                thicknessId: price.thicknessId
+                }
+            })} />
+            </CardContent>
+        </Card>
     );
 }
