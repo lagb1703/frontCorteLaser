5290dbdadc66d8fe9873ba88bcf8456ce6d6047d
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2026-02-09 00:04:43 +0000

    mejorando

--- src/shapeModule/components/shapes.tsx
+++ src/shapeModule/components/shapes.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export default function Shapes() {
     const { shapes } = useGetShape();
-    return (<main>
+    return (<main className="flex flex-col">
         {shapes?.map((shape) => {
             return (
                 <Link key={shape.id} to={`/shapes/${shape.id}`}>{shape.id}</Link>
